955c22328d4f9903b432c127a4d3d53e521080fe
Игорь Гонтаренко <igorgontaren@mail.ru>
2023-09-02 19:29:16 +0300

    [feat] - save file

--- src/main/java/org/example/FileSaver.java
+++ src/main/java/org/example/FileSaver.java
@@ -1,11 +1,57 @@
 package org.example;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 public class FileSaver
 {
-	public static void save()
-	{
+	private static String outPath;
+	private static final String TEMPLATE_NAME = "Boards Cutter template.xlsx";
 	
+	static
+	{
+		try
+		{
+			outPath = new File(FileSaver.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+			
+			if(outPath.endsWith("jar")) outPath = new File(outPath).getParent() + "\\";
+			else outPath = "C:\\Users\\Igor\\Desktop\\";
+			
+		}
+		catch(URISyntaxException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
+	
+	public static void saveWithStreams()
+	{
+		try
+		{
+			InputStream is = FileSaver.class.getResourceAsStream("/" + TEMPLATE_NAME);
+			OutputStream os = new FileOutputStream(outPath + TEMPLATE_NAME);
+			
+			byte[] buffer = new byte[1024];
+			int length;
+			while((length = is.read(buffer)) > 0)
+			{
+				os.write(buffer, 0, length);
+			}
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static boolean isFileExist()
+	{
+		return new File(outPath).exists();
+	}
+	
 	public static void noSave()
 	{
 	
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,62 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main
 {
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args)
 	{
-		System.out.println("This is Board Cutter by Igor Gontarenko");
-		System.out.println("---------------------------------------");
-		System.out.println();
-		System.out.println("Последовательность работы:");
-		System.out.println("1) Скачайте шаблон рабочего файла в ту же папку, откуда запускается программа (Board Cutter ниже предложит его скачать)");
-		System.out.println("2) Внесите необходимые данные в шаблон (Board Cutter закроется)");
-		System.out.println();
-		System.out.println("3) Запустите Board Cutter еще раз, он увидит изменения в файле (Перемещать или переименовывать файл нельзя, Board Cutter его потеряет)");
-		System.out.println("4) Продолжите работу с настройками");
-		System.out.println();
+		try
+		{
+			System.out.println("This is Board Cutter by Igor Gontarenko");
+			System.out.println("---------------------------------------");
+			System.out.println();
+			System.out.println("Последовательность работы:");
+			System.out.println("1) Скачайте шаблон рабочего файла в ту же папку, откуда запускается программа (Board Cutter ниже предложит его скачать)");
+			System.out.println("2) Внесите необходимые данные в шаблон (Board Cutter закроется)");
+			System.out.println();
+			System.out.println("3) Запустите Board Cutter еще раз, он увидит изменения в файле (Перемещать или переименовывать файл нельзя, Board Cutter его потеряет)");
+			System.out.println("4) Продолжите работу с настройками");
+			System.out.println();
+			
+			if(FileSaver.isFileExist())
+			{
+				System.out.println("Файл шаблона обнаружен, Вы находитесь на 4 пункте");
+			}else
+			{
+				System.out.println("Файл шаблона не обнаружен, Вы находитесь на 1 пункте");
+				saveOrNot();
+			}
+			
+			sc.nextLine();
+		}
+		catch(Exception e)
+		{
+			System.out.println();
+			System.out.println("FATAL EXCEPTION:");
+			e.printStackTrace();
+			sc.nextLine();
+		}
 		
-		saveOrNor();
 	}
 	
-	private static void saveOrNor()
+	private static void saveOrNot()
 	{
 		System.out.println("Скачать файл? (Y/N)");
 		System.out.print("- ");
 		
-		Scanner sc = new Scanner(System.in);
-		String answer =  sc.nextLine();
+		String answer = sc.nextLine();
 		
-		if(answer.equals("Y")) FileSaver.save();
-		else if(answer.equals("N")) FileSaver.noSave();
+		if(answer.equalsIgnoreCase("Y")) FileSaver.saveWithStreams();
+		else if(answer.equalsIgnoreCase("N")) FileSaver.noSave();
 		else
 		{
 			System.out.println();
 			System.out.println("Введите символ \"Y\" для скачаивания или \"N\" для отмены!");
-			saveOrNor();
+			saveOrNot();
 		}
 	}
 }
